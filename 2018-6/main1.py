89a964a74acda938c2bce6c14be81ccfcdcd0842
iamtehwalrus <iamtehwalrus@yahoo.com>
2018-12-20 15:19:41 -0800

    Split the input files

--- 2018-6/main1.py
+++ 2018-6/main1.py
@@ -1,5 +1,7 @@
 fpath = 'input.txt'
 contents = open(fpath,'r').read().splitlines()
 
+for i, coords in enumerate(contents):
+    contents[i] = coords.split(', ')
+    print(contents[i])
 
-print(contents)
